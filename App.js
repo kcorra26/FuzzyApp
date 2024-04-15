b5bd589f9a46ba1dc27e8b5bef5f4c0159bab19e
Kasey Corra <kaseycorra@Kaseys-MacBook-Air.local>
2024-04-15 03:00:10 -0500

    fuse ready, working on algo

--- App.js
+++ App.js
@@ -4,45 +4,60 @@ import json from './output.json';
 import {gettingDist} from './str_comp.js';
 import Fuse from 'fuse.js';
 
+const adjustedData = json.map(item => ({
+    ...item,
+    fullName: `${item["First Name"]} ${item["Last Name"]}`
+}))
+
 const options = {
-    threshold: 0.6,
-    keys: ['First Name', 'Last Name']
+    includeScore: true,
+    threshold: 0.4, // wondering how low to put this
+    keys: [ "fullName", "First Name", "Last Name" ], 
+    fieldNormWeight:2,
+    distance: 50,
+    matchAllTokens: true
 }
 
-const fuse = new Fuse(json, options);
+const fuse = new Fuse(adjustedData, options);
 
 export default function App() {
     const [searchQuery, setSearchQuery] = useState("");
     const [displayedData, setDisplayedData] = useState(json);
-    const searchableKeys = ['First Name', 'Last Name'];
+    //const searchableKeys = ['First Name', 'Last Name'];
 
-    const handleSearch = (query) => {
-        //console.log("search query:", query);
+    // Fuzzy match using a common, lightweight fuzzy-search library. 
+    // Optimized for the given database. 
+    const handleSearchFuse = (query) => {
         setSearchQuery(query);
-        // const formattedQuery = query.toLowerCase();
-        // const filteredData = json
-        //     .filter((item) => {
-        //         return gettingMatch(item, formattedQuery);
-        //     })
-        //     .map(item => {
-        //         const full_name = item['First Name'].concat(" ", item['Last Name']);
-        //         return {
-        //             ...item,
-        //             distance: (gettingDist(full_name, formattedQuery))
-        //             //distance: gettingDist(item['First Name'].concat(" ", item['Last Name']), query)
-        //         };
-        //     })
-        //     .sort((a,b) => a.distance - b.distance);
-        // setDisplayedData(filteredData);
         const result = fuse.search(query);
-        //console.log("search results:", result)
-        setDisplayedData(result.map(({ item }) => item));
+        const fraction = result.length * 0.4; // limiting the results for a faster load
+        let visibleResults = result;
+        if (fraction > 0){
+            visibleResults = result.slice(0,fraction);
+        }
+        setDisplayedData(visibleResults.map(({ item }) => item));
+    }
+
+    const handleSearchMine = (query) => {
+        setSearchQuery(query);
+        const formattedQuery = query.toLowerCase();
+        const filteredData = json
+            .filter((item) => {
+                return gettingMatch(item, formattedQuery);
+            })
+            .map(item => {
+                const full_name = item['First Name'].concat(" ", item['Last Name']);
+                return {
+                    ...item,
+                    distance: (gettingDist(full_name, formattedQuery))
+                    //distance: gettingDist(item['First Name'].concat(" ", item['Last Name']), query)
+                };
+            })
+            .sort((a,b) => a.distance - b.distance);
+        setDisplayedData(filteredData);
     }
 
     const gettingMatch = (item, query) => {
-        //return searchableKeys.some(key => item[key].toLowerCase().includes(query.toLowerCase()));
-        //return searchableKeys.some(key => gettingDist(item[key].toLowerCase(), query) <= 3);
-        //const val_to_check = 
         const full_name = item['First Name'].concat(" ", item['Last Name']);
         const lev_dist = gettingDist(full_name, query);
         //return lev_dist <= 6;
@@ -50,29 +65,30 @@ export default function App() {
         // issue is that first and last names, shorter ones are showing up bc 
     }
 
+  // Simple front end setup, with a dynamic FlatList for the search results.
   return (
     <SafeAreaView style={{flex:1,marginHorisontal:20}}>
         <TextInput 
         keyboardType="web-search"
-          placeholder='Searching'  // FIX
+          placeholder='Search'
           clearButtonMode='always' 
           style={styles.searchBox}
           autoCorrect={false}
           autoCapitalize="none"
           value={searchQuery}
-          onChangeText={handleSearch}
+          onChangeText={handleSearchFuse}
         />
 
         <FlatList 
             data={displayedData}
             scrollEnabled={true}
             keyExtractor={(item) => item["Customer Id"]} // unique key we can use from the dataset
+            //extraData={selectedId}
             renderItem={({item}) => (
                 <View style={styles.itemContainer}>
                     <Text style={styles.textName}>{item["First Name"]} {item["Last Name"]}</Text>
                     <Text style={styles.textEmail}>{item["Email"]}</Text>
                 </View>
-                // maybe some more attributes in here too (image or initials?)
             )}
         />
     </SafeAreaView>
@@ -86,11 +102,7 @@ const styles = StyleSheet.create({
     borderColor:'#ccc',
     borderWidth:1,
     borderRadius:8,
-  }, // ALL customizable -- can change later if i want
-
-//   scrollView: {
-//     marginHorizontal: 20,
-//   },
+  }, 
 
   itemContainer: {
     flexDirection: "row",
--- README.md
+++ README.md
@@ -1,4 +1,28 @@
 # Fuzzy Matching App with React Native
+__Overview__
+Assignment: Given the 1000 entries in the attached data set, do fuzzy matching filtering when a user starts typing a name in the search field in a react native app.
+
+### *Efficiency*: 
+***** IF MY OWN ALGO ******
+The gettingDist algorithm determines a basic similarity value between two strings by comparing the similarities of the substrings. Without dynamic programming (which stores the similarity value of each substring), the algorithm would complete in upwards of O(3^n) time. This is extremely inefficient. 
+To avoid a costly runtime, we use a matrix to store the similarity value at each a and b length combination. Because each value is only determined once and can then be extracted in O(1) time from our matrix, gettingDist finishes in O(n*m) time, where n and m are the lengths of strings a and b, respectively. Because this function is called on all of the names in the database, this optimization is crucial for the efficiency of our program.
+
+***** ELSE ******
+With the default Fuse implementation, the search was slow. Fuse.js is used on much larger datasets, so I knew there were ways to optimize the search in order to it to improve. 
+1) slicing the results: 
+2) threshold: 
+
+### *Optimization*:
+While Fuse.js is a functional fuzzy matching program, it left significant work to be done to optimize the search for the purpose of the assignment. How I made it better:  
+* The search was failing to rank exact matches (first and last name), and I found it was a result of the separation of the first and last name keys in the json file. To ensure that full names were identified properly, I created a modified json file with a "Full Name" key and passed that to the fuzzy matching algorithm instead. There were significant improvements.
+*  
+
+How I can make it better: 
+* implement rigorous testing so I can more quatitatively assess the effect of altering different features of the fuse program and my algorithm.
+* experiment with alternative matching libraries and run a script to compare the runtimes of one query. 
+* improve the user interface: adding clickability, loading screen, etc. Front end development was not the focus of the assignment, but it **. 
+
+
 __Working Log__
 
 ## 4/12 
@@ -21,5 +45,20 @@ __Working Log__
 * enabled scroll, added mapping and sort to order the results by relevance.
 * ran into a few problems: 
 1) just calculating the Levenstein distance between string and query using a threshold (ex. distance is 2) means that the desired name often doesn't show until far into the text (ex 'Andrew' doesn't show up when you've typed 'And' because the distance between the strings is still 3). 
-2) 
+2) ***
 * experimented with Fuse.js to compare the efficiency of an external library to what I have already written 
+
+### *To Do*:
+* decide which implementation to use for fuzzy match
+* run test cases (which one works best for our purposes)
+* add click option (highlight the name that has been clicked)
+* basic improvements to algorithm
+* export and instructions to run
+
+## 4/14
+### *Complete*: 
+* Fuse optimization: threshold testing, json modifications, documentation research 
+* Writeup complete
+
+### *To Do*:
+* export and test
